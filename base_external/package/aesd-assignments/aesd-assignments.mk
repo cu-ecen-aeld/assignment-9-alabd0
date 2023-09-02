0c7a6e2e79b4dabfc5ff881407831b376293c2cf
Alabd <oubd2013@gmail.com>
2023-09-02 03:24:16 +0300

    Assignment 5 Part 2 Complete!
    Assignment 5 Part 2 Complete!

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = a75e5260c9718d3c5eabda24869d156671cadffd
+AESD_ASSIGNMENTS_VERSION = e63ba5251b443df7d8191420b56d4a36f057ea8b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,17 +15,16 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=1 -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-#	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf/ 
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf/ $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(@D)/finder-app/finder.sh $(@D)/finder-app/finder-test.sh -t $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(@D)/finder-app/finder.sh $(@D)/finder-app/finder-test.sh \
+	$(@D)/server/aesdsocket -t $(TARGET_DIR)/usr/bin/ 
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh -T $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
